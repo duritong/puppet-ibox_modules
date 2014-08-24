d7b852634c512ad92a388c195756fd4ece622e64
mh <mh@immerda.ch>
2014-08-24 14:42:28 +0200

    more options that we might need to inject from above

--- ib_sshd/manifests/init.pp
+++ ib_sshd/manifests/init.pp
@@ -1,18 +1,20 @@
 # manage sshd
 class ib_sshd(
-  $allowed_users  = 'root',
-  $allowed_groups = '',
+  $allowed_users  = hiera('sshd::allowed_users','root'),
+  $allowed_groups = hiera('sshd::allowed_groups',''),
+  $ports          = hiera('sshd::ports',[22]),
 ) {
 
   $real_allowed_users = $allowed_groups ? {
     ''      => $allowed_users,
     default => ''
   }
-  
 
   class{'sshd':
     allowed_users    => $real_allowed_users,
     allowed_groups   => $allowed_groups,
     manage_shorewall => $ibox::use_shorewall,
+    ports            => $ports,
+    hardened_ssl     => 'no', # we would like to change that
   }
 }
