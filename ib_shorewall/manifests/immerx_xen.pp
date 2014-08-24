1873b6deffe7c30380cd2c8ca31d347dbaebd3c2
mh <mh@immerda.ch>
2014-08-24 21:43:01 +0200

    migrate a missing file

---
+++ ib_shorewall/manifests/immerx_xen.pp
@@ -0,0 +1,19 @@
+# firewall for xen hypervisors
+class ib_shorewall::immerx_xen inherits ib_shorewall::immerx_br {
+  ## base interface
+  shorewall::interface { 'xenbr0':
+    broadcast => '-',
+    zone      =>  'br',
+    options   =>  'routeback';
+  }
+
+  if ($::lsbdistcodename == 'wheezy') and ($::virtual == 'xen0') {
+    # somehow this is also needed on xen
+    shorewall::interface { 'xenbr1':
+      broadcast => '-',
+      zone      =>  'net',
+      rfc1918   => true,
+      options   =>  'tcpflags,blacklist,nosmurfs';
+    }
+  }
+}
