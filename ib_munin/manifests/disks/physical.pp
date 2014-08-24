3a272c026e9dda766fef6d2c3af26b868d1730d7
mh <mh@immerda.ch>
2014-08-24 20:13:40 +0200

    correct variable

--- ib_munin/manifests/disks/physical.pp
+++ ib_munin/manifests/disks/physical.pp
@@ -3,9 +3,9 @@ class ib_munin::disks::physical(
   $disks = false
 ) {
   if $disks {
-    $hddtemp_disks = join($physical_disks,' ')
+    $hddtemp_disks = join($disks,' ')
     munin::plugin { hddtemp_smartctl: config => "env.drives ${hddtemp_disks}\nuser root", }
-    $smartd_disks = regsubst($physical_disks,'(.*)',"smart_\\1")
+    $smartd_disks = regsubst($disks,'(.*)',"smart_\\1")
     munin::plugin { $smartd_disks: ensure => "smart_", config => "user root" }
   }
 }
