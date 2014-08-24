52f036bfac9d6feff90b84c6d70071229829affd
mh <mh@immerda.ch>
2014-08-24 15:37:27 +0200

    remove outoing sshd by default

--- ib_sshd/manifests/init.pp
+++ ib_sshd/manifests/init.pp
@@ -1,9 +1,10 @@
 # manage sshd
 class ib_sshd(
-  $allowed_users    = hiera('sshd::allowed_users','root'),
-  $allowed_groups   = hiera('sshd::allowed_groups',''),
-  $ports            = hiera('sshd::ports',[22]),
-  $manage_shorewall = hiera('sshd::manage_shorewall',$ibox::use_shorewall),
+  $allowed_users        = hiera('sshd::allowed_users','root'),
+  $allowed_groups       = hiera('sshd::allowed_groups',''),
+  $ports                = hiera('sshd::ports',[22]),
+  $manage_shorewall     = hiera('sshd::manage_shorewall',hiera('ibox::use_shorewall',false)),
+  $shorewall_out_remove = true,
 ) {
 
   $real_allowed_users = $allowed_groups ? {
@@ -17,5 +18,9 @@ class ib_sshd(
     manage_shorewall => $manage_shorewall,
     ports            => $ports,
     hardened_ssl     => 'no', # we would like to change that
+  }
+
+  if $shorewall_out_remove and $manage_shorewall {
+    include shorewall::rules::out::ssh::remove
   }
 }
