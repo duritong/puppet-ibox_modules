89c32734562a18910682b2e8f31dc03575d4cddf
mh <mh@immerda.ch>
2014-08-24 15:01:48 +0200

    make it easier to set this variable

--- ib_sshd/manifests/init.pp
+++ ib_sshd/manifests/init.pp
@@ -1,8 +1,9 @@
 # manage sshd
 class ib_sshd(
-  $allowed_users  = hiera('sshd::allowed_users','root'),
-  $allowed_groups = hiera('sshd::allowed_groups',''),
-  $ports          = hiera('sshd::ports',[22]),
+  $allowed_users    = hiera('sshd::allowed_users','root'),
+  $allowed_groups   = hiera('sshd::allowed_groups',''),
+  $ports            = hiera('sshd::ports',[22]),
+  $manage_shorewall = hiera('sshd::manage_shorewall',$ibox::use_shorewall),
 ) {
 
   $real_allowed_users = $allowed_groups ? {
@@ -13,7 +14,7 @@ class ib_sshd(
   class{'sshd':
     allowed_users    => $real_allowed_users,
     allowed_groups   => $allowed_groups,
-    manage_shorewall => $ibox::use_shorewall,
+    manage_shorewall => $manage_shorewall,
     ports            => $ports,
     hardened_ssl     => 'no', # we would like to change that
   }
