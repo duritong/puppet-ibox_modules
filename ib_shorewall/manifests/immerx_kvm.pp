8005da6cb0d04c76b7a55fbafa44027da4d15b75
mh <mh@immerda.ch>
2014-08-24 16:33:44 +0200

    linting

--- ib_shorewall/manifests/immerx_kvm.pp
+++ ib_shorewall/manifests/immerx_kvm.pp
@@ -3,8 +3,8 @@ class ib_shorewall::immerx_kvm inherits ib_shorewall::immerx_br {
   ## base interface
   shorewall::interface { 'br0':
     broadcast => '-',
-    zone    =>  'br',
-    options =>  'routeback';
+    zone      =>  'br',
+    options   =>  'routeback';
   }
 }
 
