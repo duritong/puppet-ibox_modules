7355469c5b68d777e907ea410acb1dfcd5e3c753
mh <mh@immerda.ch>
2014-08-24 16:18:04 +0200

    fix c&p errors

--- ib_sysctl/manifests/nf_conntrack.pp
+++ ib_sysctl/manifests/nf_conntrack.pp
@@ -1,14 +1,15 @@
 # raise value for connection tracking
+# of hypervisors
 class ib_sysctl::nf_conntrack(
-  max      => 65536,
-  hashsize => 65536, 
+  $max      = 65536,
+  $hashsize = 65536,
 ) {
 
   $ip_conntrack_max_key = $::osfamily ? {
     RedHat => $::operatingsystemmajrelease ? {
       '5'     => 'net.ipv4.netfilter.ip_conntrack_max',
       default => 'net.netfilter.nf_conntrack_max',
-    }
+    },
     default => 'net.ipv4.netfilter.ip_conntrack_max',
   }
 
@@ -25,8 +26,8 @@ class ib_sysctl::nf_conntrack(
     default => '/sys/module/nf_conntrack/parameters/hashsize'
   }
 
-  exec{"echo ${conntrack_hashsize_value} > ${conntrack_hashsize_file}":
-    unless => "grep -q ${conntrack_hashsize_value} ${conntrack_hashsize_file}",
+  exec{"echo ${hashsize} > ${conntrack_hashsize_file}":
+    unless => "grep -q ${hashsize} ${conntrack_hashsize_file}",
   }
 
 }
