9314c760901e79435a5acc4932295542f15866fb
mh <mh@immerda.ch>
2014-09-28 12:01:47 +0200

    these are anyway the defaults

--- ib_unbound/manifests/local_only.pp
+++ ib_unbound/manifests/local_only.pp
@@ -1,8 +1,6 @@
 # unbound for local resolv only
 class ib_unbound::local_only {
   class{'unbound':
-    interface        => hiera('unbound::interface','127.0.0.1'),
-    acls             => hiera('unbound::acls',{'127.0.0.1/32' => 'allow'}),
     manage_shorewall => false,
     manage_munin     => true,
     before           => File['/etc/resolv.conf'],
