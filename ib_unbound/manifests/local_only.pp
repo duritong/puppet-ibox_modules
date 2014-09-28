0f8ec24147b14fe931e454e730094830cc9533bd
mh <mh@immerda.ch>
2014-09-28 11:01:38 +0200

    fix path and make it possible that we can also listen on a different interface (locally)

--- ib_unbound/manifests/local_only.pp
+++ ib_unbound/manifests/local_only.pp
@@ -1,7 +1,7 @@
 # unbound for local resolv only
 class ib_unbound::local_only {
   class{'unbound':
-    interface        => '127.0.0.1',
+    interface        => hiera('unbound::interface','127.0.0.1'),
     manage_shorewall => false,
     manage_munin     => true,
     before           => File['/etc/resolv.conf'],
