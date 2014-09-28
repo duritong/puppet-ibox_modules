380e575ad69c1b06688d8fdd302e7c4175793b38
mh <mh@immerda.ch>
2014-09-28 11:03:03 +0200

    also include acl for local listening

--- ib_unbound/manifests/local_only.pp
+++ ib_unbound/manifests/local_only.pp
@@ -2,6 +2,7 @@
 class ib_unbound::local_only {
   class{'unbound':
     interface        => hiera('unbound::interface','127.0.0.1'),
+    acl              => hiera('unbound::acl',{'127.0.0.1/32' => 'allow'}),
     manage_shorewall => false,
     manage_munin     => true,
     before           => File['/etc/resolv.conf'],
