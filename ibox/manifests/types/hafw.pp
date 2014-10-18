c17ffeb9211e1f29d233a486d2a332d4c3793b77
mh <mh@immerda.ch>
2014-10-18 17:17:06 +0200

    correct function

--- ibox/manifests/types/hafw.pp
+++ ibox/manifests/types/hafw.pp
@@ -6,10 +6,18 @@ class ibox::types::hafw(
   $in_interface,
   $in_ipaddress,
   $conntrackd_interface,
-  $conntrackd_destip,
+  $conntrackd_destips,
   $net_in_rules         = {},
   $is_master            = true,
 ) {
+  include ibox
+
+  if has_ip_address($conntrackd_destips[0]) {
+    $conntrackd_destip = $conntrackd_destips[1]
+  } else {
+    $conntrackd_destip = $conntrackd_destips[0]
+  }
+
   include ib_conntrackd::hafw
   include ib_keepalived::hafw
   include ib_shorewall::hafw
