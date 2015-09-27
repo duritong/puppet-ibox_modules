a744adf4f0ba33d5fdda680bb594db41c6e35402
mh <mh@immerda.ch>
2015-09-27 11:29:16 +0200

    nagios checks must also be configurable

--- ibox/manifests/types/mailaccessproxy.pp
+++ ibox/manifests/types/mailaccessproxy.pp
@@ -1,8 +1,14 @@
 # a dovecot proxy
 class ibox::types::mailaccessproxy(
-  $sql_config = hiera('ib_dovecot::proxy'),
+  $sql_config    = hiera('ib_dovecot::proxy::sql_config'),
+  $nagios_checks = hiera('ib_dovecot::proxy::nagios_checks', {
+    'imap-hostname'  => $::fqdn,
+    'pop3-hostname'  => $::fqdn,
+    'sieve-hostname' => $::fqdn,
+  }),
 ) {
   class{'::ib_dovecot::proxy':
-    sql_config => $sql_config,
+    sql_config    => $sql_config,
+    nagios_checks => $nagios_checks,
   }
 }
