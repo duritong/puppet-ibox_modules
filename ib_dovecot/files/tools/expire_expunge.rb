12b2ebe15690aa0c518a0ac33f33c29b578a7f08
mh <mh@immerda.ch>
2015-10-08 23:10:11 +0200

    fixing new config usage

--- ib_dovecot/files/tools/expire_expunge.rb
+++ ib_dovecot/files/tools/expire_expunge.rb
@@ -17,18 +17,20 @@ class DovecotExpire
 
   private
   def conn
-    @conn ||= begin
-      f = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
-      require 'yaml'
-      config = YAML.load_file(File.join(File.dirname(f),File.basename(f,'.rb')+'.config.yaml'))
-      PGconn.connect(config['dbhost'], 5432, "","",config['dbname'], config['dbuser'],config['dbpwd'])
-    end
+    @conn ||= PGconn.connect(config['dbhost'], 5432, "","",config['dbname'], config['dbuser'],config['dbpwd'])
   end
 
   def mailboxes
     @mailboxes ||= begin
       res = conn.exec("SELECT alias||domain as email FROM email_users WHERE ismailbox = 1 AND deleted_at IS NULL AND storagehost = '#{config['fqdn']}'")
       (res.respond_to?(:values) ? res.values : res.result).flatten
+    end
+  end
+  def config
+    @config ||= begin
+      f = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
+      require 'yaml'
+      YAML.load_file(File.join(File.dirname(f),File.basename(f,'.rb')+'.config.yaml'))
     end
   end
 end
