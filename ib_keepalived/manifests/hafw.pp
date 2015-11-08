c79fe1e76d37af48d2d312f0c01ec4679b8d2f8a
mh <mh@immerda.ch>
2015-11-08 18:52:34 +0100

    fix path

--- ib_keepalived/manifests/hafw.pp
+++ ib_keepalived/manifests/hafw.pp
@@ -73,7 +73,7 @@ cat /run/keepalive.*.*.state
       },
     ],
     notify_script_master => '"/usr/libexec/keepalived/primary-backup.sh primary"',
-    notify_script_backup => '"/usr/libexec/keepalive/primary-backup.sh backup"',
+    notify_script_backup => '"/usr/libexec/keepalived/primary-backup.sh backup"',
     notify_script_fault  => '"/usr/libexec/keepalived/primary-backup.sh fault"',
     notify_script        => '"/usr/local/sbin/set_vrrp_state"',
     smtp_alert           => true,
