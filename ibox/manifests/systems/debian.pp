a3d7a7eeabdef8102c6639c339bfa7f980e69d9d
mh <mh@immerda.ch>
2016-03-27 18:36:17 +0200

    upgrade to latest apt module

--- ibox/manifests/systems/debian.pp
+++ ibox/manifests/systems/debian.pp
@@ -1,11 +1,9 @@
 # debian specific things for our environment
 class ibox::systems::debian {
-  class{'apt':
-    disable_update => true,
-  }
-  include apt::unattended_upgrades
-  include ib_apt::puppetlabs
+  include ::apt
+  include ::apt::unattended_upgrades
+  include ::ib_apt::puppetlabs
 
   # do not log ips of ssh connections
-  include loginrecords
+  include ::loginrecords
 }
