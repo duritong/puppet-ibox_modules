bd7355cbaff3eee5c658f4798f283c14fd79edec
mh <mh@immerda.ch>
2016-08-09 00:20:07 +0200

    make sure we get the uid from iuid

--- ib_apache/manifests/services/webhtpasswd.pp
+++ ib_apache/manifests/services/webhtpasswd.pp
@@ -7,6 +7,7 @@ class ib_apache::services::webhtpasswd(
       $htpasswd_name:
         domainalias  => 'absent',
         ssl_mode     => 'force',
+        uid          => iuid($htpasswd_name,'webhosting'),
         git_repo     => {
           git_repo   => 'https://git.immerda.ch/web/htpasswd.immerda.ch.git',
           submodules => true,
