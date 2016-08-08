ffbac0e00dbf231b869321c702a6718a22d46ddf
mh <mh@immerda.ch>
2016-08-09 00:32:46 +0200

    like this it's only supported on EL7

--- ib_apache/manifests/webservices.pp
+++ ib_apache/manifests/webservices.pp
@@ -3,5 +3,7 @@ class ib_apache::webservices {
   include ::ib_apache::extended
   include ::ib_apache::services::myadmin
   include ::ib_apache::services::webhtpasswd
-  include ::ib_apache::services::coquelicot
+  if versioncmp($::operatingsystemmajrelease, '6') > 0 {
+    include ::ib_apache::services::coquelicot
+  }
 }
