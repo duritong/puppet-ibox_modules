5bf685fbc43da679b63b985847f35a42c1cd5132
mh <mh@immerda.ch>
2016-11-05 16:23:37 +0100

    linting

--- ib_certs/manifests/exim.pp
+++ ib_certs/manifests/exim.pp
@@ -1,8 +1,8 @@
 # so that exim can read our internal certs
 class ib_certs::exim inherits ib_certs {
   $group = $::osfamily ? {
-    Debian  => 'Debian-exim',
-    default => 'exim',
+    'Debian' => 'Debian-exim',
+    default  => 'exim',
   }
   File['/etc/pki/tls/certs/localhost.crt','/etc/pki/tls/private/localhost.key']{
     group => $group,
